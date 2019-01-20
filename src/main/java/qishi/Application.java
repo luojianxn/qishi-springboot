17685c9e2d9a198b7c200d0bfa0535de6c4b536e
luojx <86781858@163.com>
2019-01-20 11:05:32 +0800

    20190120

--- src/main/java/qishi/Application.java
+++ src/main/java/qishi/Application.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     @RequestMapping("/")
     String home() {
-        return "hello";
+        return "hello111";
     }
 
     public static void main(String[] args) {
